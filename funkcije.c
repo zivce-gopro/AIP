65a3816999228e03e70adce24bbcea9cad597134
zivce-gopro <zivkovicmilos5@hotmail.com>
2016-05-10 16:38:11 +0200

    drugi zad

--- funkcije.c
+++ funkcije.c
@@ -6,8 +6,16 @@ int prost(int A) {
 	return p;
 }
 
+void zamena(int *a,int *b) {
+	int pom;
+	pom=*a;
+	*a=*b;
+	*b=pom;
+}
+
 
 void main() {
+	/*
 	int N,x,b=0,i;
 	printf("unesi N\n");
 	scanf("%d",&N);
@@ -17,6 +25,17 @@ void main() {
 		scanf("%d",&x);
 		if(prost(x)) b++;}//end_for
 	printf("Od %d brojeva %d brojeva je prosto\n",N,b);
+	*/ //prvi_zad_main
+
+	/* int x,y,z;
+	printf("unesi x,y,z\n");
+	scanf("%d %d %d",&x,&y,&z);
+	printf("%d %d %d\n",x,y,z);
+	zamena(&x,&y);
+	zamena(&y,&z);
+	printf("%d %d %d\n",x,y,z);*/ //drugi_zad_main
+
+
 
 
 	}
