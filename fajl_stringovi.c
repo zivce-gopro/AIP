c657f25a908e9faad2ec66544f29189b8bb942ff
zivce-gopro <zivkovicmilos5@hotmail.com>
2016-05-29 18:01:19 +0200

    prvi

--- fajl_stringovi.c
+++ fajl_stringovi.c
@@ -1,2 +1,17 @@
 #include "stdio.h"
 
+void main() {
+	/* float S=0,avg;
+	int i=0,b;
+	FILE *f = fopen("brojevi.txt","a+");
+	while(!feof(f)) {
+		fscanf(f,"%d",&b);
+		S+=b;
+		i++;
+	}
+	avg= S/i;
+	fprintf(f,"\n%.2f",avg);
+
+	fclose(f); //Prvi Zadatak */
+
+}
