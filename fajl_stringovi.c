7b6dab3b54d34a8880d4080090f99aa39e0052d8
zivce-gopro <zivkovicmilos5@hotmail.com>
2016-05-29 18:18:48 +0200

    drug

--- fajl_stringovi.c
+++ fajl_stringovi.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "string.h"
 
 void main() {
 	/* float S=0,avg;
@@ -14,4 +15,14 @@ void main() {
 
 	fclose(f); //Prvi Zadatak */
 
+	char recenica[1001]; 
+	int i=0;
+	FILE *f = fopen("reci.txt","a");
+	printf("unesi recenicu\n");
+	gets(recenica);
+	while(i < strlen(recenica)){
+		if(recenica[i] != ' ') fprintf(f,"%c",recenica[i]);
+		else fprintf(f,"\n");i++;
+	} //end_while
+	fclose(f);
 }
